da127e6ac7a93373f8773a07f610eb35f90bdee0
Guilherme <guigo_gaara@hotmail.com>
2022-05-12 19:27:14 -0300

    feat: Aadd deploy config

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT  = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/bet-you'));
+
+app.get('/*', (req,res) => {
+    res.sendFile(__dirname + '/dist/bet-you');
+});
+
+app.listen(PORT, () => {
+    console.log('Servidor iniciado na porta 8080', PORT);
+});
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    userBaseUrl: 'http://localhost:3000/api',
+    fixturesBaseUrl: 'https://api.api-futebol.com.br/v1'
+  }
 };
