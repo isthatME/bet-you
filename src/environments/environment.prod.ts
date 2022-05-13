4ab1872b719c00a806d70a76d58e8fdbce1b53b8
Guilherme <guigo_gaara@hotmail.com>
2022-05-13 17:24:41 -0300

    refactor: bet-you base url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api: {
-    userBaseUrl: 'http://localhost:3000/api',
+    userBaseUrl: 'https://bet-you-backend.herokuapp.com/api',
     fixturesBaseUrl: 'https://api.api-futebol.com.br/v1'
   }
 };
